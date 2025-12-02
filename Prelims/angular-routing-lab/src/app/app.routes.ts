fb86205b5c744f851e2f5d2d9ec54ef8d2fd79b4
alexdossss <amanabat1927@gmail.com>
2025-12-02 09:25:28 +0800

    netlify

--- Prelims/angular-routing-lab/src/app/app.routes.ts
+++ Prelims/angular-routing-lab/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Directives } from './directives/directives';
 import { DataBinding } from './data-binding/data-binding';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
+import {Tailwind} from "./tailwind/tailwind";
 
 export const routes: Routes = [
   {path:'', component: Home},
   {path:'directives', component: Directives},
-  {path:'data-binding', component: DataBinding}
+  {path:'data-binding', component: DataBinding},
+  {path:'tailwind', component: Tailwind},
+  {path:'**', component: Pagenotfound}
 ];
